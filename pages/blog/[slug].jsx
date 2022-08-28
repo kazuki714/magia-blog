096491237ed798df98f7eb9b7d75f34db26975f3
kazuki714 <funky.kazuki@gmail.com>
2022-08-28 13:17:15 +0900

    [Modify]homeに記事一覧を表示

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -103,4 +103,3 @@ export async function getStaticProps(context) {
     }
   }
 }
-
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,50 +1,23 @@
 import { getAllPosts } from "../libs/client";
 import Meta from "../components/meta";
 import Container from "../components/Container";
-import Hero from "../components/Hero";
 import Posts from "../components/posts";
-import Pagination from "../components/pagination";
-import { eyecatchLocal } from "../libs/constants";
 
 export default function Home({ posts }) {
   return (
     <Container>
       <Meta />
-      <Hero
-        title="Ayuret"
-        imageOn
-      />
       <Posts posts={posts} />
-      <Pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   );
 }
 
 export async function getStaticProps() {
-  const posts = await getAllPosts(4)
-
-  for (const post of posts) {
-    if (!post.hasOwnProperty("eyecatch")) {
-      post.eyecatch = eyecatchLocal
-    }
-    // const { base64 } = await getPlaiceholder(post.eyecatch.url)
-    // post.eyecatch.blurDataURL = base64
-  }
+  const posts = await getAllPosts()
 
   return {
     props: {
       posts: posts,
-    }
+    },
   }
 }
-
-// データをテンプレートに受け渡す部分の処理を記述します
-// export const getStaticProps = async () => {
-//   const data = await client.get({ endpoint: "blog" });
-
-//   return {
-//     props: {
-//       blog: data.contents,
-//     },
-//   };
-// };
